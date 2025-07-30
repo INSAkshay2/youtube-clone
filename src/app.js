99521fb9a20931479ec4fac6ebe5b3e454bdba36
Akshay <sharma.akshay2782629@gmail.com>
2025-07-30 12:37:44 +0530

    Controller and router

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// router import
+
+import userRouter from "./routes/user.routes.js";
+console.log("Nodemon test working!");
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
-const asynchHandler = (requestHandler) => {
-  (re, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((error) => {
       console.error("Error in async handler:", error);
       res.status(500).json({ message: "Internal Server Error" });
