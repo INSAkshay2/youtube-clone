2de026f4f29e03e341234f0832a1e0d92f4a9f10
Akshay <sharma.akshay2782629@gmail.com>
2025-07-31 19:23:31 +0530

    User Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,45 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { upload } from "../utils/multer.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  const { fullName, email, username, password } = req.body;
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = User.find({ $or: [{ email }, { username }] });
+  if (existingUser) {
+    throw new ApiError(400, "User with this email or username already exists");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath || !coverImageLocalPath) {
+    throw new ApiError(400, "Avatar and cover image are required");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage.url,
+    email,
+    password,
+    username: username.toLowerCase(),
+  })
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+  if(!createdUser) {
+    throw new ApiError(500, "User registration failed");
+  }
+  res.status(201).json(
+    new ApiResponse(201, "User registered successfully", createdUser)
+  );
 });
 
 export { registerUser };
