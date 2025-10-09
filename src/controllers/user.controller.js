15a496967705008c9016e2386a3a2aca42c1523c
Akshay <sharma.akshay2782629@gmail.com>
2025-10-09 20:01:41 +0530

    Auth-middleware done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -100,12 +100,28 @@ const loginUser = asyncHandler(async (req, res) => {
   const loggedInUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
-  res
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+  return res
     .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(new ApiResponse(200, "User logged in successfully", loggedInUser))
 
 });
+const logoutUser = asyncHandler(async(req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,{$set :{refreshToken: null}},{new:true}
+  )
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+  return res.status(200).clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(new ApiResponse(200,{},"User logged out successfully" ))
+})
 export { loginUser,registerUser,logoutUser };
 
