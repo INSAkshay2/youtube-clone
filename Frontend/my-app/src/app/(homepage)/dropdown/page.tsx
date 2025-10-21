986a4f1e694b9777a951d5b43450bce374c66d65
Akshay <sharma.akshay2782629@gmail.com>
2025-10-21 15:11:32 +0530

    feat: add DropdownMenu component and integrate into Homepage for enhanced user interaction

---
+++ Frontend/my-app/src/app/(homepage)/dropdown/page.tsx
@@ -0,0 +1,31 @@
+// file: src/components/dropdown-menu/page.tsx
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export function DropdownMenuPage() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">Open</Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent sideOffset={6}>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- Frontend/my-app/src/app/(homepage)/homepage/page.tsx
+++ Frontend/my-app/src/app/(homepage)/homepage/page.tsx
@@ -1,10 +1,15 @@
 import NavbarPage from "../navbar/page";
 import SidebarRoute from "../sidebar/page";
+import { DropdownMenuPage } from "../dropdown/page";
 export default function Homepage() {
   return (
     <div>
       <NavbarPage />
       <SidebarRoute />
+      <div className="flex bottom">
+        <DropdownMenuPage />
+      </div>
+
       <main>
         <h1>Welcome to the Homepage</h1>
       </main>
